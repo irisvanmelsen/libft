9ed2e5658a328cb549a1986d81e9e744f8b9ea62
Iris Van Melsen <ivan-mel@f0r5s19.codam.nl>
2022-10-19 15:25:08 +0200

    second commit

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,12 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_split.c                                         :+:    :+:            */
+/*   ft_itoa.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ivan-mel <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/10/19 11:49:03 by ivan-mel      #+#    #+#                 */
-/*   Updated: 2022/10/19 12:26:10 by ivan-mel      ########   odam.nl         */
+/*   Created: 2022/10/19 12:19:58 by ivan-mel      #+#    #+#                 */
+/*   Updated: 2022/10/19 14:12:24 by ivan-mel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_itoa(int n)
+{
+	char *str;
+	int	i;
+	int basenum;
+
+	i = 0;
+	basenum = 0;
+	if (!(str = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	while (n < 0)
+		str[0] = '-';
+	str[i] = '\0';
+	return (str)
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,33 +6,29 @@
 /*   By: ivan-mel <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/17 11:07:29 by ivan-mel      #+#    #+#                 */
-/*   Updated: 2022/10/19 11:48:48 by ivan-mel      ########   odam.nl         */
+/*   Updated: 2022/10/19 13:03:48 by ivan-mel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t i;
-	size_t j;
-	char *s1;
+	size_t	i;
+	size_t	j;
+	char	*s1;
 
 	i = 0;
 	j = 0;
+	if (len > ft_strlen(s))
+		len = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
 	if (!(s1 = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-//	while (i < len && start < ft_strlen(s))
-//		i++;
-//	if (!(s1 = malloc(sizeof(char) * (i + 1))))
-//		return (NULL);
-//	i = 0;
-//	if (s1 == NULL)
-//		return (NULL);
 	while (s[i] != '\0')
 	{
 		if (j < len && i >= start)
@@ -45,3 +41,8 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 	s1[j] = '\0';
 	return (s1);
 }
+/*
+int	main(void)
+{
+	ft_substr("hola", 0, 18446744073709551615);
+}*/
